11b2b4f376406ac4f7366d36960749499e37e7f7
Hanna Ramanenka <annromania@gmail.com>
2024-09-03 14:14:06 +0300

    add some activities in app

--- lab2/app/src/main/java/com/example/item/MainActivity.java
+++ lab2/app/src/main/java/com/example/item/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.item;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -12,8 +13,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn_continue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void OnContinueClick(){
+    public void OnContinueClick(View view){
         Log.i("nextStep", "OnContinueClick: ");
         Intent intent = new Intent(this, Plant.class);
         startActivity(intent);
--- lab2/app/src/main/java/com/example/item/Plant.java
+++ lab2/app/src/main/java/com/example/item/Plant.java
@@ -1,6 +1,15 @@
 package com.example.item;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +18,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Plant extends AppCompatActivity {
+
+    private TextView mInfoTextView;
+    Button btn_start_Info;
+    EditText editSpecies;
+    RadioButton typeTree, typeBush, typeGrass;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,62 @@ public class Plant extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn_start_Info = (Button) findViewById(R.id.btnStartInfo);
+        editSpecies = (EditText) findViewById(R.id.editTextPlantSpecies);
+        typeTree = (RadioButton) findViewById(R.id.radioBtnPlantType1);
+        typeBush =(RadioButton) findViewById(R.id.radioBtnPlantType2);
+        typeGrass =(RadioButton) findViewById(R.id.radioBtnPlantType3);
+        radioGroup = (RadioGroup) findViewById(R.id.typeVariety);
+        mInfoTextView = (TextView) findViewById(R.id.textViewCheck);
+
+        typeTree.setOnClickListener(radioClickListener);
+        typeBush.setOnClickListener(radioClickListener);
+        typeGrass.setOnClickListener(radioClickListener);
+
+        Bundle args = getIntent().getExtras();
+        if(args != null) {
+            editSpecies.setText(args.getString("editSpecies"));
+            mInfoTextView.setText(args.getString("TypePlant"));
+        }
     }
+
+    public void onRegisterPlant(View view) {
+        Log.i("nextStep", "onRegisterPlant: ");
+
+        Bundle args = getIntent().getExtras();
+        if (editSpecies != null) {
+            Intent intent = new Intent(this, Plant2.class);
+            intent.putExtra("editSpecies", editSpecies.getText().toString());
+            intent.putExtra("TypePlant", mInfoTextView.getText().toString());
+            if (args != null) {
+                intent.putExtra("editSpecies", args.getString("editSpecies", ""));
+                intent.putExtra("TypePlant", args.getString("TypePlant", ""));
+            }
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Введите вид растения",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void CheckBtn(View v) {
+        Log.i("checkBtn", "CheckBtn: ");
+        mInfoTextView.append(" " + editSpecies.getText().toString() + " ");
+    }
+    View.OnClickListener radioClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            RadioButton rbtn = (RadioButton) v;
+            String typePlant = "";
+            if (rbtn == typeTree){
+               typePlant = "Дерево";
+            } else if (rbtn == typeBush) {
+                typePlant = "Куст";
+            } else if (rbtn == typeGrass) {
+                typePlant = "Трава";
+            }
+            mInfoTextView.setText(typePlant);
+        }
+    };
+
 }
